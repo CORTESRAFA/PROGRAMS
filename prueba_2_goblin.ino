1c26a6cf424a703fdf1263777d65ac7414f87907
CORTESRAFA <jrafael.cortesl10@gmail.com>
2016-12-28 20:02:50 -0600

    Add files via upload

---
+++ prueba_2_goblin.ino
@@ -0,0 +1,148 @@
+#include <SoftwareSerial.h>                           
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+SoftwareSerial SIM5320(5, 4);                                                   // RX, TX
+#define ONE_WIRE_BUS 2                                                         //PIN DS18B20
+
+
+char    BUFFER_USART;                                                           //BUFFER RX 
+String  token ="gqeGtoC8soQ2fyLj2iJTQEIsloKjRdLllg5Wtzzemhjyv13dKJmUrFRp1FNM";  //TOKEN UBIDOTS
+String  id    ="5771b9b27625420dd490348e";                                      //ID DE LA VARIABLE UBIDOTS 
+
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+
+
+void setup() { 
+  pinMode(5, OUTPUT);                              //PIN PARA EL ENCENDIDO DEL SIM5320
+  SIM5320.begin(9600);
+  SIM5320_ON();                                    //ENCIENDO EL SIM
+  delay(10000);
+  SETUP_SIM5320();                                 //INICIALIZA EL SIM
+  sensors.begin();                                 
+}
+
+
+void loop() {
+
+  //
+  WEB();                                           //ENVIO JSON AL UBIDOTS
+  delay(15000);                                    //TIEMPO DE MUESTRAS
+}
+///////////////////////////////////////////////////////////////////////////
+ISR (USART_RX_vect)
+{
+  BUFFER_USART = UDR0;                               // LEE EL BUFFER RX
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void WEB()                                  
+{
+  int LONG_JSON = 15;
+  int SEND = 238;
+    
+  sensors.requestTemperatures(); 
+  float temp = sensors.getTempCByIndex(0); 
+
+  SIM5320.println(F("AT+CIPOPEN=0,\"TCP\",\"things.ubidots.com\",80"));  //
+  delay(5000);
+  
+  SIM5320.print("AT+CIPSEND=0,");  //
+  delay(50);
+  SIM5320.println(SEND);  //
+  delay(600);
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  SIM5320.print(F("POST /api/v1.6/variables/"));
+  delay(50);
+  SIM5320.print(id);
+  delay(50);
+  SIM5320.println(F("/values  HTTP/1.1"));
+  delay(50);
+  SIM5320.println(F("Content-Type: application/json"));
+  delay(50);
+  SIM5320.print(F("Content-Length:"));
+  delay(50);
+  SIM5320.println(LONG_JSON);
+  delay(50);
+  SIM5320.print(F("X-Auth-Token: "));
+  delay(50);
+  SIM5320.println(token);
+  delay(50);
+  SIM5320.println(F("Host: things.ubidots.com"));
+  delay(50);
+  SIM5320.println("");
+  ///////////////////////////////////////////////////////////////
+  SIM5320.print("{\"value\":");
+  delay(10);
+  SIM5320.print(temp,2);
+  delay(50);
+  SIM5320.println("}");
+  delay(50);
+  SIM5320.write(0x1A);
+  delay(5000);
+  SIM5320.println(F("AT+CIPCLOSE=0"));
+  delay(4000);
+
+}
+
+void SIM5320_ON()                                  //
+{
+  digitalWrite(5, HIGH);
+  delay(2000);//                            //ENCIENDE EL SIM5320
+  digitalWrite(5, LOW);
+  delay(2000);
+}
+/////////////////////////////////////////////////////////////////////////////
+void SETUP_SIM5320()
+{
+  SIM5320.println(F("AT+CFUN=1"));        //SIM5320 TRABAJANDO CON TODAS LAS FUNCIONALIDADES
+  delay(250);
+
+  SIM5320.println(F("AT+CNMP=2"));        //MODO AUTOMATICO
+  delay(250);
+
+  SIM5320.println(F("AT+CVAUXV=61"));     //VOLTAJE DE SALIDA 3.0V
+  delay(250);
+
+  SIM5320.println(F("AT+CNBP=0x0000000004000000"));  //3G 850MHZ TELCEL 0x0000000004000000 hspda     2G 1900MHZ TELCEL AT+CNBP=0x0000000000200000 edge
+  delay(500);
+
+  //SIM5320.println(F("AT+IPREX=9600"));
+  //delay(4000);
+
+  SIM5320.println(F("AT+CSQ"));            //NIVEL DE LA SEÑAL
+  delay(250);
+
+  SIM5320.println(F("AT+CREG?"));          //
+  delay(250);
+
+  SIM5320.println(F("AT+COPS?"));
+  delay(250);
+ 
+  SIM5320.println(F("AT+CGSOCKCONT=1,\"IP\",\"internet.itelcel.com\""));
+  delay(250);
+
+  SIM5320.println(F("AT+CNSMOD?"));        //MUESTRA EN QUE RED ESTA TRABAJANDO
+  delay(250);
+
+  SIM5320.println(F("AT+CSOCKSETPN=1"));
+  delay(250);
+
+  SIM5320.println(F("AT+CIPMODE=0"));
+  delay(250);
+
+  SIM5320.println(F("AT+CNBP?"));
+  delay(250);
+
+  SIM5320.println(F("AT+NETOPEN"));
+  delay(8000);
+
+  SIM5320.println(F("AT+IPADDR"));          //IP
+  delay(500);
+
+  SIM5320.println(F("AT+CGSN"));            //IMEI
+  delay(500);
+}
